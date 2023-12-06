87f221b0bef925cea64dd17dba8addcb7daf36de
Arshiya Hashmi <111113095+AH9906@users.noreply.github.com>
2023-12-06 21:20:47 +0400

    Update stats.c

--- Applications Assignments/Week 4/src/stats.c
+++ Applications Assignments/Week 4/src/stats.c
@@ -1,137 +1,127 @@
-/******************************************************************************
- * Copyright (C) 2017 by Alex Fosdick - University of Colorado
- *
- * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
- * permitted to modify this and use it to learn about the field of embedded
- * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
- *
- *****************************************************************************/
-/**
- * @file stats.c
- * @brief The implementation file of the c1m1 assignment
- *
- * This file includes all the required functions implementations in the c1m1 assignment
- *
- * @author Mohammed Abdelalim
- * @date 7/9/2020
- * @edited 24/10/2020  
- *
- */
-
-
-
 #include <stdio.h>
-#include "../include/common/stats.h"
-#include "../include/common/platform.h"
+#include <stdint.h>
+#include "platform.h"
+#include "stats.h"
 
-/* Size of the Dataset */
+
+/* Size of the Data Set */
 #define SIZE (40)
 
-/* void main() {
+// void main() {
 
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
-                              114, 88,   45,  76, 123,  87,  25,  23,
-                              200, 122, 150, 90,   92,  87, 177, 244,
-                              201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};
+//   unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+//                               114, 88,   45,  76, 123,  87,  25,  23,
+//                               200, 122, 150, 90,   92,  87, 177, 244,
+//                               201,   6,  12,  60,   8,   2,   5,  67,
+//                                 7,  87, 250, 230,  99,   3, 100,  90,};
 
-  // Other Variable Declarations Go Here 
-  unsigned char minimum = 0;
-  unsigned char maximum = 0;
-  float mean = 0;
-  unsigned char median = 0;
+//   /* Other Variable Declarations Go Here */
+//   /* Statistics and Printing Functions Go Here */
 
-  Statistics and Printing Functions Go Here 
-  PRINTF("Array before sorting: \n");
-  print_array(&test, SIZE);
-  median = find_median(&test, SIZE);
-  mean = find_mean(&test, SIZE);
-  maximum = find_maximum(&test, SIZE);
-  minimum = find_minimum(&test, SIZE);
-  print_statistics(minimum, maximum, mean, median);
-  sort_array(&test, SIZE);
-  PRINTF("Array after sorting: \n");
-  print_array(&test, SIZE);
-} */
+//   // Create Struct
+// stat Array;
 
-void print_statistics (unsigned char minimum, unsigned char maximum, float mean, unsigned char median){
-  printf("The minimum is %d \n", minimum);
-  printf("The maximum is %d \n", maximum);
-  printf("The mean is %f \n", mean);
-  printf("The median is %d \n", median);
+//   // Initialize Struct
+//   Array.numArray = test;
+//   Array.arrayLength = SIZE;
+//   Array.max = 0;
+//   Array.min = 0xFF;
+//   Array.mean = 0;
+//   Array.median = 0;
+
+//   // Process Data
+//   sort_array(Array.numArray, Array.arrayLength);
+//   Array.min = find_min(Array.numArray, Array.arrayLength);
+//   Array.max = find_max(Array.numArray, Array.arrayLength);
+//   Array.mean = find_mean(Array.numArray, Array.arrayLength);
+//   Array.median = find_median(Array.numArray, Array.arrayLength);
+//   print_array(Array.numArray, Array.arrayLength);
+//   print_statistics(Array);
+// }
+
+void print_statistics(uint8_t *arrayPtr, uint8_t arrayLength){
+    print_array(arrayPtr, arrayLength);
+    PRINTF("Array Min: %u\n", find_min(arrayPtr, arrayLength));
+    PRINTF("Array Max: %u\n", find_max(arrayPtr, arrayLength));
+    PRINTF("Array Mean: %u\n", find_mean(arrayPtr, arrayLength));
+    PRINTF("Array Median: %u\n", find_median(arrayPtr, arrayLength));
+    PRINTF("Array Size: %u\n", arrayLength);
+    print_array(arrayPtr, arrayLength);
+    return;
 }
 
-
-void print_array (unsigned char *array, unsigned int counter){
-  // The function is required to work only if the user defines the "VERBOSE" macro in command line interface.
+void print_array(uint8_t *arrayPtr, uint8_t arrayLength){
   #ifdef VERBOSE
-    for (int i=0; i<counter; i++){
-      PRINTF("%d \t ", *(array + i));
+    // Loop through and print the array
+    for(uint8_t i=0; i<arrayLength; i++){
+      PRINTF("array[%u](0x%x): %u\n", i, arrayPtr+i, *(arrayPtr + sizeof(char)*i));
     }
     PRINTF("\n");
   #endif
+  return;
 }
 
-
-unsigned char find_median (unsigned char *array, unsigned int counter){
-  unsigned char median = 0;
-  median = *(array + (counter / 2) -1);
-  return median;
-}
-
-float find_mean (unsigned char *array, unsigned int counter){
-  unsigned int accumulator = 0; // variable to store the accumulator value throughout the mean finding process
-  float mean = 0;
-  for (int i=0; i<counter; i++){
-    accumulator = accumulator + array[i] /* *(array + i) */;
-  }
-  printf("acc = %d \n", accumulator);
-  mean = accumulator / ((float) counter); //must type cast one of the two integers to float for accurate calculation.
-  return mean;
-}
-
-unsigned char find_maximum (unsigned char *array, unsigned int counter){
-  unsigned char maximum = *array; // initalize the maximum variable with the value of the first array element
-  for (int i=1; i<counter; i++){ // start the loop from the second elemnt
-    if (*(array + i) > maximum){
-      maximum = *(array + i);
-    }
-    else {} // do nothing.
-  }
-  return maximum;
-}
-
-unsigned char find_minimum (unsigned char *array, unsigned int counter){
-  unsigned char minimum = *array; // initalize the minimum variable with the value of the first array element
-  for (int i=1; i<counter; i++){ // start the loop from the second elemnt
-    if (*(array + i) < minimum){
-      minimum = *(array + i);
-    }
-    else {} // do nothing.
-  }
-  return minimum;
-}
-
-void sort_array (unsigned char *array, unsigned int counter){
-  char flag = 0; //this flag is to indicate if a swap process has occurred at least once in the loop
-  unsigned char temp;
-  do {
-    flag =0; //set flag to default at every new 'do' operation
-    for (int index=0; index<counter; index++){
-      if (array[index] > array[index +1] || array[index] == array[index +1]) {
-        continue;
-      }
-      else if (array[index] < array[index +1]) {
-        //swap elements//
-        temp = array[index];
-        array[index] = array[index+1];
-        array[index+1] = temp;
-
-        flag = 1; // a swap process has occurred at least once in this loop
+void sort_array(uint8_t *arrayPtr, uint8_t arrayLength){
+  uint8_t i, j = 0;
+  uint8_t temp;
+  // Sort Array
+  for(i=0; i<arrayLength; i++){
+    for(j=i+1; j<arrayLength; j++){
+      if(arrayPtr[i]<arrayPtr[j]){
+        temp = arrayPtr[i];
+        arrayPtr[i]=arrayPtr[j];
+        arrayPtr[j]=temp;
       }
     }
   }
-  while (flag ==1); // the array is not sorted as long as a swap operation is occurred at least once
+}
+
+uint8_t find_min(uint8_t *arrayPtr, uint8_t arrayLength){
+  uint8_t minValue=0xFF;
+
+  for (uint8_t i=0; i<arrayLength; i++){
+    if (arrayPtr[i] < minValue){
+      minValue = arrayPtr[i];
+    }
+  }
+
+  return minValue;
+}
+
+uint8_t find_max(uint8_t *arrayPtr, uint8_t arrayLength){
+  uint8_t maxValue=0;
+
+  for (uint8_t i=0; i<arrayLength; i++){
+    if (arrayPtr[i] > maxValue){
+      maxValue = arrayPtr[i];
+    }
+  }
+
+  return maxValue;
+}
+
+uint8_t find_mean(uint8_t *arrayPtr, uint8_t arrayLength){
+  uint32_t avg=0;
+
+  // Add up all the numbers in the array
+  for(uint8_t i=0; i<arrayLength; i++){
+    avg+= arrayPtr[i];
+  }
+  
+  // Integer division using Array Length
+  return (avg/arrayLength);
+}
+
+uint8_t find_median(uint8_t *arrayPtr, uint8_t arrayLength){
+  // Make sure array is sorted first
+  sort_array(arrayPtr, arrayLength);
+
+  // Case where array length is odd
+  if (arrayLength%2){
+    return arrayPtr[arrayLength/2];
+  }
+
+  // Case where array length is even.  Average between two middle values is necessary
+  uint8_t avg = (arrayPtr[arrayLength/2 - 1]+arrayPtr[arrayLength/2])/2;
+  return avg;
 }
