abd90a5555265028d62c6525230be9399a58a8a6
Arshiya Hashmi <111113095+AH9906@users.noreply.github.com>
2023-12-06 21:18:39 +0400

    Update data.c

--- Applications Assignments/Week 4/src/data.c
+++ Applications Assignments/Week 4/src/data.c
@@ -1,93 +1,95 @@
-/**
- * @file data.c
- * @brief Abstraction of the basic data manipulation operations
- *
- * This implementation file provides an abstraction of the basic data manipulation operations
- * like converting integer to string and vice versa via function calls.
- * 
- * @author Mohammed Abdelalim
- * @date 26/10/2020
- *
- */
+#include <stdio.h>
+#include "data.h"
+
+uint8_t my_itoa(int32_t data, uint8_t * ptr, uint32_t base){
+    /* Since the min/max value of data is ±2147483648, we need a max array size of
+     * 12, 1 for sign, 10 for digits, 1 for null terminator */
+    uint8_t arraySize = 12;
+    uint8_t tempArray[arraySize];
+    uint8_t isNegativeFlag = 0;
+    uint8_t strLength = 0;
+
+    // Case where number is zero;
+    if (data == 0){
+        *ptr='0';
+        *(ptr+1)=0;
+        return 2;
+    }
+
+    // case of a negative number
+    if (data<0){
+        data = data*-1;
+        isNegativeFlag = 1;
+    }
+    // Null character
+    tempArray[strLength] = 0x0;
+    strLength++;
+
+    // Peel off each character
+    while (data >= 1){
+        uint8_t myChar = data%base;
+        // printf("data%%base: %d\n", myChar);
+        
+        // If char is 0-9
+        if(myChar>=0 && myChar<=9){
+            tempArray[strLength] = myChar + 0x30;
+        }
+
+        // If char is A-F
+        if(myChar>=10 && myChar<=15){
+            tempArray[strLength] = myChar + 0x37;
+        }
+
+        data = data/base;
+        strLength++;
+    }
+
+    // Add negative sign if negative number
+    if(isNegativeFlag){
+        tempArray[strLength] = '-';
+        strLength++;
+    }
+
+    // Since string number is assembled in reverse, flip it for the string.
+    for(uint8_t i=0 ; i<strLength; i++){
+        *(ptr+i)=tempArray[strLength-1-i];
+    }
 
 
-#include "../include/common/memory.h"
-#include "../include/common/data.h"
-#include "../include/common/platform.h"
-#include <stdint.h>
-#include <stddef.h>
-
-uint8 my_itoa(int32 data, uint8 * ptr, uint32 base)
-{
-  uint8 isNegative= 0;
-  uint8 digitCounter= 0;
-  uint8 remainder= 0;  
-
-  // Check if data is zero
-  if (data == 0)
-  {
-    *(ptr) = '0';
-    *(ptr + 1) = '\0';
-    return 2;
-  }
-  
-  // Check if data is negative
-  if (data < 0)
-  {
-    isNegative = 1;
-    data = data*-1;
-  }  
-  // Convert int to ASCII for all bases
-  while (data != 0)
-  {
-    remainder = data % base;
-    *(ptr + digitCounter)= ( remainder > 9 ) ? ( remainder - 10 ) + 'a' : remainder + '0';
-    digitCounter++;
-    data= data / base;
-  }  
-  // Add -ve sign if negative data
-  if (isNegative)
-  {
-    *(ptr + digitCounter) = '-';
-    digitCounter++;
-  }  
-  digitCounter++;
-  *(ptr + digitCounter) = '\0';
-
-  // Reverse the string index order  
-  my_reverse(ptr, (digitCounter - 1));
-  
-  return digitCounter;
+    return strLength;
 }
 
-int32 my_atoi(uint8 * ptr, uint8 digits, uint32 base)
-{
-  int32 number = 0;
-  uint8 isNegative = 0;
 
-  // Check sign
-  if(*ptr == '-')
-  {
-    isNegative = 1;
-    // Move on to the next digit
-    ptr++;
-    // One less digit to deal with
-    digits--;
-  }
+int32_t my_atoi(uint8_t * ptr, uint8_t digits, uint32_t base){
+    uint8_t isNegativeFlag = 0;
+    uint32_t myNumber=0;
+    uint8_t *charPtr = ptr;
 
-  digits--;
+    if(*charPtr == '-'){
+        isNegativeFlag = 1;
+        digits--;
+        charPtr++;
+    }
 
-  for(int i = 0; i < digits; i++)
-  {
-    number = number * base + *ptr - '0';
-    ptr++;
-  }
+    // While not Null Character, add values
+    uint8_t i = 0;
+    do {
+        uint32_t multiplier = pow(base,(digits-2-i));
+        uint8_t digit = *(charPtr+i);
+        if(digit<='F' && digit>='A'){
+            digit-=0x37;
+        }
+        else if(digit<='9' && digit>='0'){
+            digit-=0x30;
+        }
+        // printf("Adding %d to myNumber\n", multiplier*digit);
+        myNumber+= multiplier*digit;
+        i++;
+    } while(*(charPtr+i-1) != 0);
 
-  // Apply the negative sign if needed
-  if(isNegative)
-  {
-    number = -number;
-  }
-
-  return number;
+    if(isNegativeFlag){
+        myNumber*=-1;
+    }
+    
+    return myNumber;
 }
