c99a6fc32e4fc98458698c24fefb5c8ba553dc25
Arshiya Hashmi <111113095+AH9906@users.noreply.github.com>
2023-12-06 21:25:30 +0400

    Update platform.h

--- Applications Assignments/Week 4/include/common/platform.h
+++ Applications Assignments/Week 4/include/common/platform.h
@@ -15,7 +15,7 @@
  Platform - MSP432
 ******************************************************************************/
 #if defined (MSP432)
-#include "../msp432/msp432p401r.h"
+#include "msp432p401r.h"
 #define PRINTF(...)
 /******************************************************************************
  Platform - HOST
@@ -23,11 +23,6 @@
 #elif defined (HOST)
 #include <stdio.h>
 #define PRINTF(...) printf(__VA_ARGS__)
-/* The dots are called, together with the __VA_ARGS__, variadic macros
-* When the macro is invoked, all the tokens in its argument list [...],
-* including any commas, become the variable argument.
-* This sequence of tokens replaces the identifier VA_ARGS in the macro body wherever it appears.
-*/
 /******************************************************************************
  Platform - Unsupported
 ******************************************************************************/
