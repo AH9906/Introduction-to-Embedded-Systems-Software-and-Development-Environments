da829a3cfefecd3c36e49511a0ac73dee2ed1c71
Arshiya Hashmi <111113095+AH9906@users.noreply.github.com>
2023-12-06 21:12:40 +0400

    Update data.h

--- Applications Assignments/Week 4/include/common/data.h
+++ Applications Assignments/Week 4/include/common/data.h
@@ -1,57 +1,50 @@
-/**
- * @file data.h
- * @brief Abstraction of some basic data manipulation
+/******************************************************************************
+ * Copyright (C) 2023 by Arshiya Hashmi
  *
- * This header file provides an abstraction of the basic data manipulation,
- * converting data from int to ASCII, and vice-versa
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain the copyright found in 
+ * the LICENSE file.
  *
- * @author Mohammed Abdelalim
- * @date 24/10/2020
+ * THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE
  *
- */
+ *****************************************************************************/
 
-#ifndef __data_H__
-#define __data_H__
+#ifndef __DATA_H__
+#define __DATA_H__
 
+#include <stdint.h>
 #include <math.h>
 
 /**
- * @brief Converts given data from integer to ASCII string
- *
- * Given a pointer to a char dataset, this will set a provided
- * index into that dataset to the value provided.
- * Regarding the signed numbers, the function handles the base 10 signed numbers only.
- *
- * @param data The int number to be converted
- * @param ptr Pointer to data array
- * @param base Base to convert to
- *
- * @return length of the converted data.
- */
-uint8 my_itoa(int32 data, uint8 * ptr, uint32 base);
-
-/**
- * @brief Converts given data from ASCII string to integer
- *
- * Given a pointer to a char dataset, this will set a provided
- * index into that dataset to the value provided.
- * The function handles the base 10 signed numbers.
- *
- * @param ptr Pointer to the string to be converted
- * @param digits Number of digits in the character set
- * @param value Base to convert to
- *
- * @return the converted integer.
- */
-int32 my_atoi(uint8 * ptr, uint8 digits, uint32 base);
-
-/**
- * @brief Calculates the power of a nymber
+ * @brief Integer-to-ASCII function.
  * 
- * @param number The base number
- * @param power The power factor
- *
- * @return the result.
+ * Converts an data from a standard integer type into an ASCII string, supports
+ * a variety of numbers and bases.
+ * 
+ * @param data The 32-bit, signed number to be converted to ASCII
+ * @param ptr The pointer to the output string
+ * @param base The base of the number passed in, support base between 2 to 16
+ * @return uint8_t Returns the length of the converted data (including negative sign & null terminator)
  */
-int32 powerFunction(int32 number, uint8 power);
+uint8_t my_itoa(int32_t data, uint8_t * ptr, uint32_t base);
+
+
+/**
+ * @brief ASCII-to-Integer function.
+ * 
+ * Converts a ASCII string to a number with a specified base
+ * 
+ * @param ptr Points to the beggining of the character string to convert
+ * @param digits Number of digits in the string
+ * @param base The base of the number, bases between 2 to 16 are supported
+ * @return int32_t Converted 32-bit signed integer
+ */
+int32_t my_atoi(uint8_t * ptr, uint8_t digits, uint32_t base);
+
 #endif
